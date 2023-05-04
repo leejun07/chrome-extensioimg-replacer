f6ffb0334823a0b90062a77838c3ac0b1cddff7b
Jun <junealee07@gmail.com>
2023-05-04 12:25:44 -0700

    random dog generator

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+// chrome.runtime.onMessage.addListener(async function () {
+//   if (message.msg === "image") {
+//     const getImage = await fetch('https://dog.ceo/api/breeds/image/random')
+//     const processedResponse = await getImage.json();
+//     senderResponse({data: processedResponse, index: message.index})
+//     return true;
+//   }
+// });
+
+
 // chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
 //   if (message.msg === "image") {
 //     fetch('https://dog.ceo/api/breeds/image/random')
@@ -7,15 +17,22 @@
 //             senderResponse({data: dataObj, index: message.index});
 //           })
 //           .catch(error => console.log("error", error))
-//       return true;  // Will respond asynchronously.
+//       return true;
 //   }
-// });
+// })
 
-chrome.runtime.onMessage.addListener(async function(message, sender, senderResponse){
-    if (message.msg === "image") {
-      const getImage =  await fetch('https://dog.ceo/api/breeds/image/random')
-      const processedResponse = await promise.json();
-      console.log(processedResponse.message);
-      return true;
-    }
-  });
+
+async function loadDogs() {
+  const imgPage = document.querySelector('.image')
+  const getImage = await fetch('https://dog.ceo/api/breeds/image/random');
+  const processedResponse = await getImage.json();
+  imgPage.src = processedResponse.message;
+  console.log(processedResponse);
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => loadDogs());
+
+
+
+
